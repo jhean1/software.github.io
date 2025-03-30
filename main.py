6d72b40562dc5786deecca7975e3e45bd8cf51d8
jhean1 <jheancarlosvilladiego@gmail.com>
2025-03-30 17:41:19 -0500

    "

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("hola mundo")
-print("hola")
+import time
+
+print("Bienvenido al menu")
+time.sleep(2)
+print("seleccione la opción que desea realizar")
+print("1. Agregar")
+print("2. Modificar")
+print("3. Eliminar")
+print("4. Mostrar")
+print("5. salir del programa")
+x=int(input(""))
+
+
+
+
+
