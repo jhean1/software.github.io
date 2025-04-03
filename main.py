20c8ad83336666a1b17d6287a98f3c50c91dba61
jhean1 <jheancarlosvilladiego@gmail.com>
2025-04-02 20:04:14 -0500

    Corrige errores en main.py relacionados con la validación de datos

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ import time
 init(autoreset=True)
 
 while True:
-    # Crear marco superior
-    print(Fore.CYAN + '=' * 50)
-    print(Fore.CYAN + 'BIENVENIDO AL MENU'.center(50))
-    print(Fore.CYAN + '=' * 50)
-    time.sleep(1)
+    # Crear marco decorativo superior
+    print(Fore.CYAN + '+' + '=' * 48 + '+')
+    print(Fore.CYAN + '|' + 'BIENVENIDO AL MENU'.center(48) + '|')
+    print(Fore.CYAN + '+' + '=' * 48 + '+')
 
     # Opciones del menú
-    print(Fore.YELLOW + "Seleccione la opción que desea realizar:".center(50))
-    print(Fore.GREEN + "1. Agregar".center(50))
-    print(Fore.BLUE + "2. Modificar".center(50))
-    print(Fore.RED + "3. Eliminar".center(50))
-    print(Fore.MAGENTA + "4. Mostrar".center(50))
-    print(Fore.WHITE + "5. Salir del programa".center(50))
-    print(Fore.CYAN + '=' * 50)  # Línea decorativa inferior
+    print(Fore.YELLOW + '|' + "Seleccione la opción que desea realizar:".center(48) + '|')
+    print(Fore.GREEN + '|' + "1. Agregar".center(48) + '|')
+    print(Fore.BLUE + '|' + "2. Modificar".center(48) + '|')
+    print(Fore.RED + '|' + "3. Eliminar".center(48) + '|')
+    print(Fore.MAGENTA + '|' + "4. Mostrar".center(48) + '|')
+    print(Fore.WHITE + '|' + "5. Salir del programa".center(48) + '|')
+    print(Fore.CYAN + '+' + '=' * 48 + '+')  # Línea decorativa inferior
 
     try:
         x = int(input(Fore.CYAN + "Ingrese su opción: ".center(50)))
@@ -26,6 +25,7 @@ while True:
         print(Fore.RED + "Por favor, ingrese un número válido.".center(50))
         continue
 
+    # Respuesta según la opción seleccionada
     if x == 1:
         print(Fore.GREEN + "Seleccionaste agregar".center(50))
     elif x == 2:
@@ -40,4 +40,5 @@ while True:
     else:
         print(Fore.RED + "Opción no válida, intenta de nuevo.".center(50))
     
-    print(Style.DIM + '=' * 50)  # Línea decorativa para separar iteraciones
+    # Separador entre iteraciones del menú
+    print(Style.DIM + '+' + '=' * 48 + '+')
