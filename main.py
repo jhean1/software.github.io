ff9395fa810f9691562fdd12bec58a49b87df318
jhean1 <jheancarlosvilladiego@gmail.com>
2025-04-02 19:27:50 -0500

    Corrige errores en main.py relacionados con la validación de datos

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import time
 
 print("Bienvenido al menu")
-time.sleep(2)
+time.sleep(1)
 print("seleccione la opción que desea realizar")
 print("1. Agregar")
 print("2. Modificar")
@@ -10,6 +10,21 @@ print("3. Eliminar")
 print("4. Mostrar")
 print("5. salir del programa")
 x=int(input(""))
+
+if x==1:
+    print("seleccionaste agregar")
+elif x==2:
+    print("seleccionaste modificar")
+elif x==3:
+    print("seleccionaste eliminar")
+elif x==4:
+    print("seleccinaste mostrar")
+else:
+    print("saliste del programa")
+    exit()
+
+
+
 
 
 
