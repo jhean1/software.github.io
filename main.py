af79ac162752b62c1b157223b80f44ba2e79bd07
jhean1 <jheancarlosvilladiego@gmail.com>
2025-04-02 19:48:14 -0500

    Corrige errores en main.py relacionados con la validación de datos# Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-while True:
-    print('BIENVENIDO AL MENU' .center(50, '='))
-    import time
-
-    
-    time.sleep(1)
-    print("seleccione la opción que desea realizar")
-    print("1. Agregar")
-    print("2. Modificar")
-    print("3. Eliminar")
-    print("4. Mostrar")
-    print("5. salir del programa")
-    x=int(input(""))
-
-    if x==1:
-         print("seleccionaste agregar")
-    elif x==2:
-        print("seleccionaste modificar")
-    elif x==3:
-        print("seleccionaste eliminar")
-    elif x==4:
-        print("seleccinaste mostrar")
-    else:
-        print("saliste del programa")
-        exit()
-    print('' .center(50, '='))
-
 from colorama import Fore, Style, init
 import time
 
